62504619eef02e42f0168557dff2ad868594ed75
Sukant Kumar <suka1557@gmail.com>
2019-12-01 22:39:41 +0530

    Try6

--- open_nmt_api_call.py
+++ open_nmt_api_call.py
@@ -19,7 +19,7 @@ f.close()
 
 #subprocess.call(["onmt_translate", "-model" , "open_nmt_default_model_step_100000.pt" , "-src" , "input.txt" , "-output" , "output.txt", "-replace_unk", "-verbose" ], shell = True)
 
-subprocess.call("onmt_translate"+"-model open_nmt_default_model_step_100000.pt -src input.txt -output output.txt -replace_unk -verbose", shell=True)
+subprocess.call("onmt_translate"+" -model open_nmt_default_model_step_100000.pt -src input.txt -output output.txt -replace_unk -verbose", shell=True)
 
 of = open(output_file, 'r+')
 spanish_sentence = of.read()
